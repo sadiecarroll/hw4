2baca44e3f8ad85883d82bb7a6b820728f872fe1
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-02-29 16:14:22 +0000

    updated sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    flash["notice"] = "Welcome, #{user.username}!"
+    redirect_to "/places"
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
   
