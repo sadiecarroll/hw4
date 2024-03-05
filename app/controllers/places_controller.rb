565db5f3c82ecc238ab5d069184b038f66131e19
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-03-05 16:59:12 +0000

    places connected to login

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,13 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @place = Place.where({ "user_id" => session["user_id"] })
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
     @entries = Entry.where({ "place_id" => @place["id"]})
+    
   end
 
   def new
@@ -14,10 +15,12 @@ class PlacesController < ApplicationController
   end
 
   def create
+    if User.find_by({ "id" => session["user_id"] }) != nil
     @place = Place.new
     @place["name"] = params["name"]
+    @place["user_id"] = session["user_id"]
     @place.save
     redirect_to "/places"
   end
-
+end
 end
