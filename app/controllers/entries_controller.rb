0f3758c46abf628aa27f00248060baa2a98cebd4
Sadie Carroll <sadie.carroll@kellogg.northwestern.edu>
2024-03-08 01:05:50 +0000

    updated controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -16,10 +16,17 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
-    # @entry.uploaded_image.attach(params["uploaded_image"])
+    @entry.uploaded_image.attach(params["uploaded_image"])
     @entry["user_id"] = @user["id"]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
